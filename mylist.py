bcb25134b69af4994590ce5fb450ca39f9ce71a8
qianzhiqiang024 <36885082+qianzhiqiang024@users.noreply.github.com>
2018-02-28 20:10:21 +0800

    Update mylist.py

--- mylist.py
+++ mylist.py
@@ -10,15 +10,11 @@ print mylist1[0:2]
 mylist4 = ["List item 1", 3, 3.14]
 print mylist1[0:2]
 
-
-mylist4 = ["List item 1", 3, 3.14]
+mylist5 = ["List item 1", 3, 3.14]
 print mylist1[0:2]
 
-
-mylist4 = ["List item 1", 3, 3.14]
+mylist6 = ["List item 1", 3, 3.14]
 print mylist1[0:2]
 
-
-
-mylist4 = ["List item 1", 3, 3.14]
+mylist7 = ["List item 1", 3, 3.14]
 print mylist1[0:2]

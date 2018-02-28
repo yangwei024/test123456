0f9d69fe65d21dbbf53059fd4cead69a5909eb97
qianzhiqiang024 <36885082+qianzhiqiang024@users.noreply.github.com>
2018-02-28 20:08:25 +0800

    Update mylist.py

--- mylist.py
+++ mylist.py
@@ -1,14 +1,24 @@
 mylist = ["List item 1", 3, 3.14]
 print mylist[0:2]
 
-mylist1 = ["List item 1", 3, 3.14]
-print mylist1[0:2]
-
 mylist2 = ["List item 1", 3, 3.14]
 print mylist1[0:2]
 
 mylist3 = ["List item 1", 3, 3.14]
 print mylist1[0:2]
+
+mylist4 = ["List item 1", 3, 3.14]
+print mylist1[0:2]
+
+
+mylist4 = ["List item 1", 3, 3.14]
+print mylist1[0:2]
+
+
+mylist4 = ["List item 1", 3, 3.14]
+print mylist1[0:2]
+
+
 
 mylist4 = ["List item 1", 3, 3.14]
 print mylist1[0:2]
